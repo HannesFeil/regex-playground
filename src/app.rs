9eeccddad82382c236e1aa89c6079ed3f53c9aaf
HannesFeil <hannesfeil@gmx.de>
2024-06-27 19:56:30 +0200

    Implement logging and simple input

--- src/app.rs
+++ src/app.rs
@@ -1,38 +1,84 @@
 use std::io;
 
 use crossterm::event;
-use ratatui::widgets::{Block, Paragraph, Widget};
+use log::trace;
+use ratatui::{
+    layout::{Constraint, Layout},
+    text::ToLine,
+    widgets::{Block, Paragraph, Row, Widget},
+};
+use tui_input::Input;
 
 use crate::tui::Tui;
 
 pub struct App {
     running: bool,
+    regex_input: Input,
+    data: String,
+    focus_state: FocusState,
+}
+
+enum FocusState {
+    TypingRegex,
 }
 
 impl App {
-    pub fn new() -> Self {
-        App { running: true }
+    pub fn new(data: String) -> Self {
+        App {
+            running: true,
+            regex_input: Input::new("awesome regex".to_owned()),
+            data,
+            focus_state: FocusState::TypingRegex,
+        }
     }
 
     pub fn run(&mut self, terminal: &mut Tui) -> io::Result<()> {
         while self.running {
-            self.handle_events();
-            terminal.draw(|frame| frame.render_widget(&mut *self, frame.size()))?;
+            terminal.draw(|frame| {
+                frame.render_widget(&mut *self, frame.size());
+                if let Some((cursor_x, cursor_y)) = match self.focus_state {
+                    FocusState::TypingRegex => Some((1 + self.regex_input.visual_cursor() as u16, 1)),
+                } {
+                    frame.set_cursor(cursor_x, cursor_y);
+                }
+            })?;
+            self.handle_events()?;
         }
         Ok(())
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            event::Event::Key(key_event) => self.handle_key_event(key_event),
+        let event = event::read()?;
+        trace!("Got event {event:?}");
+        let input = tui_input::backend::crossterm::to_input_request(&event);
+        trace!("Input would be: {input:?}");
+        match event {
+            event::Event::Key(key_event) => self.handle_key_event(key_event, input),
             event::Event::Paste(string) => todo!(),
             _ => {}
         }
         Ok(())
     }
 
-    fn handle_key_event(&mut self, key_event: event::KeyEvent) {
-        
+    fn handle_key_event(
+        &mut self,
+        key_event: event::KeyEvent,
+        input: Option<tui_input::InputRequest>,
+    ) {
+        match key_event {
+            event::KeyEvent {
+                code: event::KeyCode::Char('q'),
+                kind: event::KeyEventKind::Press,
+                ..
+            } => {
+                self.running = false;
+            }
+            _ => {
+                if let Some(input) = input {
+                    self.regex_input.handle(input);
+                }
+            }
+        }
     }
 }
 
@@ -41,9 +87,13 @@ impl Widget for &mut App {
     where
         Self: Sized,
     {
-        Paragraph::new("Test text woop")
-            .centered()
+        let [input_area, preview_area] =
+            Layout::vertical([Constraint::Length(3), Constraint::Min(3)]).areas(area);
+        Paragraph::new(self.regex_input.to_line())
             .block(Block::bordered())
-            .render(area, buf);
+            .render(input_area, buf);
+        Paragraph::new(self.data.as_str())
+            .block(Block::bordered())
+            .render(preview_area, buf);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-use std::io;
+use std::{
+    io::{self, Read, Write},
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 use app::App;
 use clap::Parser;
+use crossterm::tty::IsTty;
+use log::info;
 
 mod app;
 mod tui;
 
 #[derive(Parser)]
-struct Args {}
+struct Args {
+    pub data: Option<PathBuf>,
+}
+
+struct Log(Arc<Mutex<Vec<u8>>>);
+
+impl Log {
+    fn new() -> Self {
+        Log(Arc::default())
+    }
+
+    fn inspect(&self, f: impl FnOnce(&[u8])) {
+        f(&self.0.lock().unwrap())
+    }
+}
+
+impl Write for Log {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.lock().unwrap().write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.lock().unwrap().flush()
+    }
+}
+
+impl Clone for Log {
+    fn clone(&self) -> Self {
+        Log(self.0.clone())
+    }
+}
 
 fn main() -> io::Result<()> {
-    Args::parse();
+    let args = Args::parse();
+
+    let log = Log::new();
+    env_logger::builder()
+        .target(env_logger::Target::Pipe(Box::new(log.clone())))
+        .filter_level(log::LevelFilter::max())
+        .init();
 
     let hook = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |panic_info| {
@@ -19,13 +61,30 @@ fn main() -> io::Result<()> {
         hook(panic_info)
     }));
 
-    let mut app = App::new();
-
     let mut terminal = tui::init()?;
 
+    let input = match args.data {
+        Some(path) => std::fs::read_to_string(path)?,
+        None => if io::stdin().is_tty() {
+            String::new()
+        } else {
+            let mut string = String::new();
+            io::stdin().read_to_string(&mut string)?;
+            string
+        },
+    };
+    let mut app = App::new(input);
+
+    info!("Running application");
     app.run(&mut terminal);
+    info!("Application exit");
 
     tui::restore()?;
+
+    log.inspect(|data| {
+        io::stdout().write_all(data).unwrap();
+        io::stdout().flush().unwrap();
+    });
 
     Ok(())
 }
